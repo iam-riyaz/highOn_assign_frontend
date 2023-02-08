f21d8e597631b381ab030b229675a4356bc2b9d6
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2023-02-08 10:39:06 +0530

    map adding try

--- highOn_frontend/src/components/Map_page.jsx
+++ highOn_frontend/src/components/Map_page.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+
+import {GoogleMap,useLoadScript,Marker} from "@react-google-maps/api"
+
 
 export const Map_page = () => {
+  
+  
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
 
-  
 
   const getLocation = async () => {
     if (navigator.geolocation) {
@@ -13,7 +17,7 @@ export const Map_page = () => {
       alert("geolocation not available in this browser");
     }
   };
-  setTimeout(getLocation,2000)
+  setTimeout(getLocation, 2000);
 
   const showLocation = (position) => {
     let long = position.coords.longitude;
@@ -31,16 +35,47 @@ export const Map_page = () => {
     console.log(longitude, latitude);
   }, [latitude, longitude]);
 
+
+  const {isLoaded}=useLoadScript({
+    googleMapsApiKey:"AIzaSyAIA_zqjFMsJM_sxP9-6Pde5vVCTyJmUHM",
+  })
+  if(!isLoaded)
+  {
+    return (<>
+    <h2>loading..........</h2>
+    </>)
+  }
+  
+
+
   return (
     <>
-      <h2>this is user map page</h2>
+    <h2>map</h2>
+       <GoogleMap zoom={10} center={{lat:44,lng:-80}}></GoogleMap>
       
     </>
   );
 };
 
-<script
+
+
+const showMap= ()=>{
+
+  return(
+    <>
+    <GoogleMap zoom={10} center={{lat:44,lng:-80}}></GoogleMap>
+    </>
+  )
+
+}
+
+
+
+
+
+
+{/* <script
   async
   defer
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIA_zqjFMsJM_sxP9-6Pde5vVCTyJmUHM&callback=initMap"
-></script>;
+></script>; */}
